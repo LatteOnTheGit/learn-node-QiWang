4afa1561142ceb381074d245867f72adbbb0e719
Latte <bjwq2019@gmail.com>
2024-02-26 13:31:20 -0500

    quiz

--- basics/quiz/helloserver.js
+++ basics/quiz/helloserver.js
@@ -9,15 +9,15 @@ const hostname = config.hostname;
 const port = config.port;
 
 const server = http.createServer((req, res) => {
-    if(req.url && req.url.endsWith('/home')) {
+    if(req.url && (req.url.endsWith('/home'))) {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Welcome to home!');
     }
     else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Invalid request due to bad URL');
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({hostname, port}));
     }
     
 })
